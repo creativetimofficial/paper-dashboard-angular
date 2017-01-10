50226701c3e9c07f669c555c8e2ea7a137776633
chelaruc <chelaru_ciprian@yahoo.com>
2017-01-10 12:28:44 +0200

    Added Documentation

--- assets/js/paper-dashboard.js
+++ assets/js/paper-dashboard.js
@@ -1,4 +1,5 @@
 var fixedTop = false;
+var transparent = true;
 
 var navbar_initialized = false;
 
@@ -6,14 +7,18 @@ var mobile_menu_initialized = false,
     toggle_initialized = false,
     bootstrap_nav_initialized = false,
     mobile_menu_visible = 0;
-    
+
 $(document).ready(function(){
     window_width = $(window).width();
+    
+    $('#menuresize a').click(function(){
+        var href = $(this).attr('href');
+        $('html,body').animate({
+            'scrollTop': $($(this).attr('href')).offset().top - 100
+        }, 200);
+    })
 
-    // Init navigation toggle for small screens
-    if(window_width <= 991){
-        pd.initRightMenu();
-    }
+    pd.initSidebarCheck();
 
     //  Activate the tooltips
     $('[rel="tooltip"]').tooltip();
@@ -22,16 +27,37 @@ $(document).ready(function(){
 
 // activate collapse right menu when the windows is resized
 $(window).resize(function(){
-    if($(window).width() <= 991){
-        pd.initRightMenu();
-    }
+    pd.initSidebarCheck();
 });
 
 pd = {
     misc:{
         navbar_menu_visible: 0
     },
-
+    checkScrollForTransparentNavbar: debounce(function() {
+            if($(document).scrollTop() > 381 ) {
+                if(transparent) {
+                    transparent = false;
+                    $('.navbar-color-on-scroll').removeClass('navbar-transparent');
+                    $('.navbar-title').removeClass('hidden');
+                }
+            } else {
+                if( !transparent ) {
+                    transparent = true;
+                    $('.navbar-color-on-scroll').addClass('navbar-transparent');
+                    $('.navbar-title').addClass('hidden');
+                }
+            }
+    }, 17),
+    initSidebarCheck: function(){
+        if($(window).width() <= 991){
+            if(($(".sidebar").length) != 0){
+                pd.initRightMenu();
+            } else{
+                pd.initBootstrapNavbarMenu();
+            }
+        }
+    },
     initRightMenu: debounce(function(){
         $sidebar_wrapper = $('.sidebar-wrapper');
 
@@ -127,7 +153,71 @@ pd = {
 
             toggle_initialized = true;
         }
-    }, 200)
+    }, 200),
+    initBootstrapNavbarMenu: debounce(function(){
+        if(!bootstrap_nav_initialized){
+            $navbar = $('nav').find('.navbar-collapse').first().clone(true);
+
+            nav_content = '';
+            mobile_menu_content = '';
+            //add the content from the regular header to the mobile menu
+            $navbar.children('ul').each(function(){
+                content_buff = $(this).html();
+                nav_content = nav_content + content_buff;
+            });
+
+            nav_content = '<ul class="nav nav-mobile-menu">' + nav_content + '</ul>';
+            $navbar.html(nav_content);
+            $navbar.addClass('bootstrap-navbar');
+
+            // append it to the body, so it will come from the right side of the screen
+            $('body').append($navbar);
+            $toggle = $('.navbar-toggle');
+            $navbar.find('a').removeClass('btn btn-round btn-default');
+            $navbar.find('button').removeClass('btn-round btn-fill btn-info btn-primary btn-success btn-danger btn-warning btn-neutral');
+            $navbar.find('button').addClass('btn-simple btn-block');
+
+            $toggle.click(function (){
+                if(mobile_menu_visible == 1) {
+                    $('html').removeClass('nav-open');
+                    $('.close-layer').remove();
+
+                    setTimeout(function(){
+                        $toggle.removeClass('toggled');
+                    }, 400);
+                    mobile_menu_visible = 0;
+                } else {
+                    setTimeout(function(){
+                        $toggle.addClass('toggled');
+                    }, 430);
+
+                    $layer = $('<div class="close-layer"></div>');
+                    $layer.appendTo(".wrapper-full-page");
+
+                    setTimeout(function(){
+                        $layer.addClass('visible');
+                    }, 100);
+
+                    $layer.click(function() {
+                        $('html').removeClass('nav-open');
+
+                        mobile_menu_visible = 0;
+                        $layer.removeClass('visible');
+
+                        setTimeout(function(){
+                            $layer.remove();
+                            $toggle.removeClass('toggled');
+                        }, 400);
+                    });
+
+                    $('html').addClass('nav-open');
+                    mobile_menu_visible = 1;
+                }
+            });
+
+            bootstrap_nav_initialized = true;
+        }
+    }, 500)
 }
 
 
