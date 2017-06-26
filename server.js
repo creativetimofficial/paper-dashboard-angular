8dc5f4e4bb3078612b44ad31c0fa8bbb3753c19a
Axelut <axelut@me.com>
2017-06-26 15:52:15 +0300

    removed HTTPS

--- server.js
+++ server.js
@@ -2,24 +2,6 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-// If an incoming request uses
-// a protocol other than HTTPS,
-// redirect that request to the
-// same url but with HTTPS
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(['https://', req.get('Host'), req.url].join(''));
-    }
-    next();
-  }
-}
-
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
-
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
