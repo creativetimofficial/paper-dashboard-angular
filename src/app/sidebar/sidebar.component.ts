0b9a1ead3b669b6361f4ef4bb6c40314f62a46f7
chelaruc <chelaruciprian10@gmail.com>
2019-07-12 14:29:25 +0300

    99% finished sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -9,14 +9,14 @@ export interface RouteInfo {
 }
 
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'ti-panel', class: '' },
-    { path: '/user', title: 'User Profile',  icon:'ti-user', class: '' },
-    { path: '/table', title: 'Table List',  icon:'ti-view-list-alt', class: '' },
-    { path: '/typography', title: 'Typography',  icon:'ti-text', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'ti-pencil-alt2', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'ti-map', class: '' },
-    { path: '/notifications', title: 'Notifications',  icon:'ti-bell', class: '' },
-    { path: '/upgrade', title: 'Upgrade to PRO',  icon:'ti-export', class: 'active-pro' },
+    { path: '/dashboard',     title: 'Dashboard',         icon:'nc-bank',       class: '' },
+    { path: '/icons',         title: 'Icons',             icon:'nc-diamond',    class: '' },
+    { path: '/maps',          title: 'Maps',              icon:'nc-pin-3',      class: '' },
+    { path: '/notifications', title: 'Notifications',     icon:'nc-bell-55',    class: '' },
+    { path: '/user',          title: 'User Profile',      icon:'nc-single-02',  class: '' },
+    { path: '/table',         title: 'Table List',        icon:'nc-tile-56',    class: '' },
+    { path: '/typography',    title: 'Typography',        icon:'nc-caps-small', class: '' },
+    { path: '/upgrade',       title: 'Upgrade to PRO',    icon:'nc-spaceship',  class: 'active-pro' },
 ];
 
 @Component({
