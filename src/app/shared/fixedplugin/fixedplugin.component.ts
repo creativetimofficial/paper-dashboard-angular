7a3adef75dc815b93e213d7da6924db1972edda1
chelaruc <chelaruciprian10@gmail.com>
2019-07-16 10:28:08 +0300

    added fixed-plugin

--- src/app/shared/fixedplugin/fixedplugin.component.ts
+++ src/app/shared/fixedplugin/fixedplugin.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-declare var $:any;
-
 @Component({
     moduleId: module.id,
     selector: 'fixedplugin-cmp',
@@ -9,58 +7,26 @@ declare var $:any;
 })
 
 export class FixedPluginComponent implements OnInit{
-    ngOnInit(){
-        var $sidebar = $('.sidebar');
-        var $off_canvas_sidebar = $('.off-canvas-sidebar');
-        var window_width = $(window).width();
 
-        if(window_width > 767){
-            if($('.fixed-plugin .dropdown').hasClass('show-dropdown')){
-                $('.fixed-plugin .dropdown').addClass('open');
-            }
+  public sidebarColor: string = "white";
+  public sidebarActiveColor: string = "danger";
 
-        }
+  public state: boolean = true;
 
-        $('.fixed-plugin a').click(function(event){
-          // Alex if we click on switch, stop propagation of the event, so the dropdown will not be hide, otherwise we set the  section active
-            if($(this).hasClass('switch-trigger')){
-                if(event.stopPropagation){
-                    event.stopPropagation();
-                }
-                else if(window.event){
-                   window.event.cancelBubble = true;
-                }
-            }
-        });
+  changeSidebarColor(color){
+    var sidebar = <HTMLElement>document.querySelector('.sidebar');
 
-        $('.fixed-plugin .background-color span').click(function(){
-            $(this).siblings().removeClass('active');
-            $(this).addClass('active');
-
-            var new_color = $(this).data('color');
-
-            if($sidebar.length != 0){
-                $sidebar.attr('data-background-color',new_color);
-            }
-
-            if($off_canvas_sidebar.length != 0){
-                $off_canvas_sidebar.attr('data-background-color',new_color);
-            }
-        });
-
-        $('.fixed-plugin .active-color span').click(function(){
-            $(this).siblings().removeClass('active');
-            $(this).addClass('active');
-
-            var new_color = $(this).data('color');
-
-            if($sidebar.length != 0){
-                $sidebar.attr('data-active-color',new_color);
-            }
-
-            if($off_canvas_sidebar.length != 0){
-                $off_canvas_sidebar.attr('data-active-color',new_color);
-            }
-        });
+    this.sidebarColor = color;
+    if(sidebar != undefined){
+        sidebar.setAttribute('data-color',color);
     }
+  }
+  changeSidebarActiveColor(color){
+    var sidebar = <HTMLElement>document.querySelector('.sidebar');
+    this.sidebarActiveColor = color;
+    if(sidebar != undefined){
+        sidebar.setAttribute('data-active-color',color);
+    }
+  }
+  ngOnInit(){}
 }
--- src/app/shared/fixedplugin/fixedplugin.module.ts
+++ src/app/shared/fixedplugin/fixedplugin.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FixedPluginComponent } from './fixedplugin.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-    imports: [ RouterModule, CommonModule ],
+    imports: [ RouterModule, CommonModule, NgbModule ],
     declarations: [ FixedPluginComponent ],
     exports: [ FixedPluginComponent ]
 })
