7fa4b401619f0e8d8012cb494616c131c5a248c2
chelaruc <chelaruciprian10@gmail.com>
2019-07-17 11:04:52 +0300

    changed sidebar color

--- src/app/shared/fixedplugin/fixedplugin.component.ts
+++ src/app/shared/fixedplugin/fixedplugin.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class FixedPluginComponent implements OnInit{
 
-  public sidebarColor: string = "black";
+  public sidebarColor: string = "white";
   public sidebarActiveColor: string = "danger";
 
   public state: boolean = true;
