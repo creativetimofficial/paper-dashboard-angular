c20180c3ae990152baf5f3b39e322417cc9dc91b
JoseFelipe2024 <jfelipec@miumg.edu.gt>
2025-02-08 07:48:26 -0600

    Mejora

--- src/app/pages/icons/image-carousel.ts
+++ src/app/pages/icons/image-carousel.ts
@@ -1,39 +1,67 @@
-import { Component, HostListener, ElementRef, Renderer2 } from '@angular/core';
+import { Component, ElementRef, Renderer2, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-image-carousel',
   templateUrl: './image-carousel.component.html',
   styleUrls: ['./image-carousel.component.css']
 })
-export class ImageCarouselComponent {
+export class ImageCarouselComponent implements OnInit {
   images = [
-    { src: 'assets/Imagenes/michi.webp', alt: 'Imagen de un michi' },
-    { src: 'assets/Imagenes/paisaje.webp', alt: 'Hermoso paisaje' },
+    { src: 'assets/Imagenes/michi.webp', alt: 'Imagen de un michi', duration: 3000 },
+    { src: 'assets/Imagenes/paisaje.webp', alt: 'Hermoso paisaje', duration: 5000 },
   ];
   
   currentIndex = 0;
   isFullscreen = false;
+  interval: any;
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  constructor(private el: ElementRef, private renderer: Renderer2, private router: Router) {}
 
-  toggleFullscreen() {
-    this.isFullscreen = !this.isFullscreen;
+  ngOnInit() {
+    this.startCarousel();
+    this.enterFullscreen();
+  }
 
+  startCarousel() {
+    this.updateCarousel();
+  }
+
+  updateCarousel() {
+    clearInterval(this.interval);
+    this.interval = setInterval(() => {
+      this.nextImage();
+    }, this.images[this.currentIndex].duration);
+  }
+
+  nextImage() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    this.updateCarousel();
+  }
+
+  prevImage() {
+    this.currentIndex = (this.currentIndex === 0) ? this.images.length - 1 : this.currentIndex - 1;
+    this.updateCarousel();
+  }
+
+  enterFullscreen() {
     const carousel = this.el.nativeElement.querySelector('.carousel-container');
-
-    if (this.isFullscreen) {
-      if (carousel.requestFullscreen) {
-        carousel.requestFullscreen();
-      }
-    } else {
-      if (document.exitFullscreen) {
-        document.exitFullscreen();
-      }
+    if (carousel.requestFullscreen) {
+      carousel.requestFullscreen().then(() => this.isFullscreen = true);
     }
   }
 
-  @HostListener('document:fullscreenchange', [])
-  onFullscreenChange() {
-    this.isFullscreen = !!document.fullscreenElement;
+  exitFullscreen() {
+    if (document.fullscreenElement) {
+      document.exitFullscreen().then(() => this.isFullscreen = false);
+    }
+  }
+
+  toggleFullscreen() {
+    if (!this.isFullscreen) {
+      this.enterFullscreen();
+    } else {
+      this.exitFullscreen();
+    }
   }
 }
